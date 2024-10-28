3e919bec960fb97f1c28a037c8c321d137440455
jiiyeon <bego9205@gmail.com>
2024-10-28 21:07:33 +0900

    Modified : modify osi_bridge_main for python

--- src/async_stream/src/osi_bridge_main_python.py
+++ src/async_stream/src/osi_bridge_main_python.py
@@ -6,18 +6,17 @@ import threading
 def run():
   osi_bridge.ros_init('grpc_client')
 
-  # host_name = sys.argv[0]
   host_name = "localhost:50051"
   cmd_host_name = "localhost:50052"
   keti_ros_bridge = osi_bridge.KetiROSBridge(host_name, cmd_host_name)
 
+  bridge_thread = threading.Thread(target=keti_ros_bridge.StartBridge,  args=())
   client_thread = threading.Thread(target=keti_ros_bridge.ClientStartListen, args=())
   server_thread = threading.Thread(target=keti_ros_bridge.ServerStartStream, args=())
-  bridge_thread = threading.Thread(target=keti_ros_bridge.StartBridge,  args=())
 
+  bridge_thread.start()
   client_thread.start()
   server_thread.start()
-  bridge_thread.start()
 
 if __name__ == '__main__':
   run()
